71ae76e01937bebed71c8ef8fcd09c799c428769
Patrick <scheichpatrick@gmail.com>
2021-06-17 12:25:39 +0200

    Aufgaben Vektoren

---
+++ VL04/VL04_Aufgaben/VL04_Aufgaben/Vektoren.cpp
@@ -0,0 +1,40 @@
+// VL04_Aufgaben.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+void printVec(std::vector<std::string> food)
+{
+    for (auto entry : food)
+        std::cout << entry << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<std::string> food = { "Eier", "Milch", "Zucker", "Schokolade", "Mehl" };
+
+    printVec(food);
+
+    food.erase(food.begin());
+
+    printVec(food);
+
+    food.push_back("Kaffee");
+
+    printVec(food);
+
+     auto zucker = std::find_if(food.begin(), food.end(), [](std::string s) {
+       return s == "Zucker";
+        });
+
+     if(zucker != food.end())
+        *zucker = "Honig";
+
+    printVec(food);
+
+    return 0;
+}
